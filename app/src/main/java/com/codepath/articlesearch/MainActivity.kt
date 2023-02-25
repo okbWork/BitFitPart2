152fc2716e53d9c1dbb7cbecca0e2b6986684b37
Kob-Land <62191780+Kob-Land@users.noreply.github.com>
2023-02-24 22:22:51 -0500

    Finished Project

--- app/src/main/java/com/codepath/articlesearch/MainActivity.kt
+++ app/src/main/java/com/codepath/articlesearch/MainActivity.kt
@@ -3,12 +3,16 @@ package com.codepath.articlesearch
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.codepath.articlesearch.databinding.ActivityMainBinding
 import com.codepath.asynchttpclient.AsyncHttpClient
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.launch
 import kotlinx.serialization.json.Json
 import okhttp3.Headers
 import org.json.JSONException
@@ -25,25 +29,76 @@ private const val ARTICLE_SEARCH_URL =
     "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=${SEARCH_API_KEY}"
 
 class MainActivity : AppCompatActivity() {
-    private val articles = mutableListOf<Article>()
+    private val articles = mutableListOf<DisplayArticle>()
     private lateinit var articlesRecyclerView: RecyclerView
     private lateinit var binding: ActivityMainBinding
+    lateinit var swipeContainer: SwipeRefreshLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
-
-
-
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
+        setContentView(R.layout.activity_main)
+
+        // Lookup the swipe container view
+
+        swipeContainer = findViewById(R.id.swiperefresh)
+
+        swipeContainer.setOnRefreshListener {
+            articlesRecyclerView = findViewById(R.id.articles)
+
+            val articleAdapter = ArticleAdapter(this, articles)
+            articlesRecyclerView.adapter = articleAdapter
+
+
+            lifecycleScope.launch {
+                (application as ArticleApplication).db.articleDao().getAll().collect { databaseList ->
+                    databaseList.map { entity ->
+                        DisplayArticle(
+                            entity.headline,
+                            entity.articleAbstract,
+                            entity.byline,
+                            entity.mediaImageUrl
+                        )
+                    }.also { mappedList ->
+                        articles.clear()
+                        articles.addAll(mappedList)
+                        articleAdapter.notifyDataSetChanged()
+                    }
+                }
+            }
+            swipeContainer.isRefreshing=false
+        }
+
+
+
+
         articlesRecyclerView = findViewById(R.id.articles)
 
         val articleAdapter = ArticleAdapter(this, articles)
         articlesRecyclerView.adapter = articleAdapter
+
+
+        lifecycleScope.launch {
+            (application as ArticleApplication).db.articleDao().getAll().collect { databaseList ->
+                databaseList.map { entity ->
+                    DisplayArticle(
+                        entity.headline,
+                        entity.articleAbstract,
+                        entity.byline,
+                        entity.mediaImageUrl
+                    )
+                }.also { mappedList ->
+                    articles.clear()
+                    articles.addAll(mappedList)
+                    articleAdapter.notifyDataSetChanged()
+                }
+            }
+        }
+
 
 
         articlesRecyclerView.layoutManager = LinearLayoutManager(this).also {
@@ -60,6 +115,15 @@ class MainActivity : AppCompatActivity() {
                 throwable: Throwable?
             ) {
                 Log.e(TAG, "Failed to fetch articles: $statusCode")
+                if (statusCode == 404) {
+                    Log.e(TAG, "Error $statusCode: Failed to launch. Reqest not Found.")
+                } else if (statusCode == 400) {
+                    Log.e(TAG, "Error $statusCode: Failed to launch. Bad Gateway.")
+                } else if (statusCode == 402) {
+                    Log.e(TAG, "Error $statusCode: Failed to launch. Access Denied.")
+                } else if (statusCode == 403) {
+                    Log.e(TAG, "Error $statusCode: Failed to launch. Unauthorized Request.")
+                }
             }
 
             override fun onSuccess(statusCode: Int, headers: Headers, json: JSON) {
@@ -71,9 +135,24 @@ class MainActivity : AppCompatActivity() {
                         json.jsonObject.toString()
                     )
 
+
+
                     parsedJson.response?.docs?.let { list ->
-                        articles.addAll(list)
+                        lifecycleScope.launch(IO) {
+                            (application as ArticleApplication).db.articleDao().deleteAll()
+                            (application as ArticleApplication).db.articleDao().insertAll(list.map {
+                                ArticleEntity(
+                                    headline = it.headline?.main,
+                                    articleAbstract = it.abstract,
+                                    byline = it.byline?.original,
+                                    mediaImageUrl = it.mediaImageUrl
+                                )
+                            })
+                        }
                     }
+
+
+
 
                     articleAdapter.notifyDataSetChanged()
                 } catch (e: JSONException) {
@@ -84,4 +163,6 @@ class MainActivity : AppCompatActivity() {
         })
 
     }
+
+
 }
